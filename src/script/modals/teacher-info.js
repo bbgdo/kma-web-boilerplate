95d26194a2530fa96fc8c545c8d5ebc02330d5ff
chucho <bohdan.tarverdiiev@gmail.com>
2025-10-04 06:23:53 +0300

    task 1 almost done

--- src/script/modals/teacher-info.js
+++ src/script/modals/teacher-info.js
@@ -8,6 +8,9 @@ const createTeacherInfoModal = (user) => {
     const email = !isNull(user.email)
         ? `<a href="mailto: ${user.email}">${user.email}</a>`
         : `<span>no email</span>`;
+    const favouriteButton = user.favorite
+        ? `<button class="standard-button" id="mark-favourite" type="button">Unmark favourite</button>`
+        : `<button class="standard-button" id="mark-favourite" type="button">Mark favourite</button>`
 
     return `
     <div class="teacher-card-modal-wrapper">
@@ -24,7 +27,10 @@ const createTeacherInfoModal = (user) => {
         </div>
     </div>
     <p class="teacher-info-modal-description">${user?.note ?? ''}</p>
-    <a href="#" class="toggle-map-modal">toggle map</a>
+    <div class="teacher-info-wrapper-bottom">
+        <a href="#" class="toggle-map-modal">toggle map</a>
+        ${favouriteButton}
+    </div>
 `
 }
 
@@ -37,12 +43,24 @@ const openTeacherInfoModal = (user) => {
 document.addEventListener('click', (e) => {
     const card = e.target.closest('.teacher-card[data-id]');
     if (!card) return;
-    const user = usersValidated.users.find(u => u.id === card.dataset.id);
-    openTeacherInfoModal(user);
+    openTeacherInfoModal(usersValidated.users.find(u => u.id === card.dataset.id));
 });
 
 document.addEventListener('teacherInfo:open', (e) => {
     const { user } = e.detail;
     const modal = document.querySelector('.teacher-info-modal-body');
-    modal.innerHTML = createTeacherInfoModal(user);
+    const renderModal = () => {
+        modal.innerHTML = createTeacherInfoModal(user);
+
+        // need a proper way to access "db"(json file) and update the user
+        const favBtn = modal.querySelector('#mark-favourite');
+        if (favBtn) {
+            favBtn.addEventListener('click', () => {
+                user.favorite = !user.favorite;
+                renderModal();
+            });
+        }
+    };
+
+    renderModal();
 });
